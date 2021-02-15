908345ba8c87a7ed5b8f803d1116f3229314aaa2
Osama yousef <fotboy1788@hotmail.com>
2021-02-15 10:26:02 +0200

    add last step

--- prework/arrow-functions/my-starter-code/app.js
+++ prework/arrow-functions/my-starter-code/app.js
@@ -212,17 +212,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//  it represent the object that we defined before 
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// it will return undefined 
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// arrow function use this as window or global object they inherit it from perant scope 
